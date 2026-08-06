0168969da3957894c777fdf321ab420b933521ab
Darshan Lila <23020201093@darshan.ac.in>
2026-08-06 10:24:57 +0530

    Enhance Pascal triangle program with validation
    Added input validation and leading spaces for formatting.

--- 19.c
+++ 19.c
@@ -1,3 +1,5 @@
+// WAP to print Pascal triangle
+
 #include <stdio.h>
 
 int main()
@@ -5,18 +7,21 @@ int main()
     int rows;
 
     printf("Enter number of rows: ");
-    scanf("%d", &rows);
+    if (scanf("%d", &rows) != 1 || rows <= 0) {
+        printf("Invalid input\n");
+        return 1;
+    }
 
-    for (int i = 0; i < rows; i++)
-    {
+    for (int i = 0; i < rows; i++) {
         int num = 1;
+        /* print leading spaces for triangle shape */
+        for (int s = 0; s < rows - i - 1; s++)
+            printf("  ");
 
-        for (int j = 0; j <= i; j++)
-        {
-            printf("%d ", num);
+        for (int j = 0; j <= i; j++) {
+            printf("%4d", num);
             num = num * (i - j) / (j + 1);
         }
-
         printf("\n");
     }
 
